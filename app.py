0c75d36fab9452f5a8004f4e616b38ca8dbc11e3
marcosvile <marcosvilelateixeira@gmail.com>
2021-12-08 11:30:37 -0300

    alterando as div de todas as pag

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/explorar")
+@app.route("/")
 def explorar():
     return render_template('explorar.html')
 
