1567497c5b81974e5d4e4f1700a65b2ca7465a0d
marcosvile <marcosvilelateixeira@gmail.com>
2021-12-08 10:59:03 -0300

    criando bkend flask do explorar

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/")
+@app.route("/explorar")
 def explorar():
     return render_template('explorar.html')
 
