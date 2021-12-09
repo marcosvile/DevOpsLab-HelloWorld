75481272dd7cfef057852c300f25aa9de4f5bd81
marcosvile <marcosvilelateixeira@gmail.com>
2021-12-09 11:11:30 -0300

    app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/")
-def index():
+@app.route("/index")
+def explorar():
     return render_template('explorar.html')
 
 if __name__ == '__main__':
