25f29bfe79c3403230c9f36d9881c0c492b3006f
marcosvile <marcosvilelateixeira@gmail.com>
2021-12-09 11:56:11 -0300

    app.py

--- app.py
+++ app.py
@@ -10,5 +10,17 @@ def index():
 def explorar():
     return render_template('explorar.html')
 
+@app.route("/contato.html")
+def contato():
+    return render_template('contato.html')
+
+@app.route("/github.html")
+def github():
+    return render_template('github.html')
+
+@app.route("/roadmap.html")
+def roadmap():
+    return render_template('roadmap.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
