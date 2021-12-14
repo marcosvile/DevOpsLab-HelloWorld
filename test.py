acf78e271ae1e480f216a1a8e9746fd103a73c53
marcosvile <marcosvilelateixeira@gmail.com>
2021-12-14 07:30:21 -0300

    alterando o test

--- test.py
+++ test.py
@@ -9,14 +9,14 @@ class Test(unittest.TestCase):
 
     def test_requisicao(self):
         # envia uma requisicao GET para a URL
-        result = self.app.get('/')
+        result = self.app.get('/index.html')
 
         # compara o status da requisicao (precisa ser igual a 200)
         self.assertEqual(result.status_code, 200) 
 
     def test_conteudo(self):
         # envia uma requisicao GET para a URL
-        result = self.app.get('/') 
+        result = self.app.get('/index.hmtl') 
 
         # verifica o retorno do conteudo da pagina
         self.assertRegex(result.data.decode(), "Bem Vindo a página Guia do Dev")
