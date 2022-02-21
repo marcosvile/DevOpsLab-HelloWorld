ea1f175921def1e93ae06a6a8c802e1d69714468
marcosvile <marcosvilelateixeira@gmail.com>
2022-02-21 13:05:13 -0300

    commit

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def index():
     return render_template('index.html')
 
 # @app.route("/index.html") #rota para o index
-# def index1():
+# def index2():
 #     return render_template('index.html')
 
 @app.route("/explorar.html")
